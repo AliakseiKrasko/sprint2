e178680e801b24c24abb6b1e86129fb79983df90
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-04 19:24:17 +0100

    added disabled buttons at click

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -63,6 +63,7 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
+                        disabled={(info === '...loading')}
                         // дописать
 
                     >
@@ -72,6 +73,7 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
+                        disabled={(info === '...loading')}
                         // дописать
 
                     >
@@ -81,6 +83,7 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
+                        disabled={(info === '...loading')}
                         // дописать
 
                     >
@@ -90,6 +93,7 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
+                        disabled={(info === '...loading')}
                         // дописать
 
                     >
